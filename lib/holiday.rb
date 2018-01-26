c8811c7a1ca23b7b4d43bbe79c6ec50c81e88412
Stephen Thurman <thurman024@gmail.com>
2018-01-26 05:02:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, data|
+    puts "#{season}"
+    data.each do |holiday, supply_list|
+      puts "#{holiday}: #{holiday.values}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
